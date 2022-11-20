ccfa19332c6cdb7b1b2a29e15d7b97471c30a79c
Igor Pelevin <ia_pelevin@mail.ru>
2022-11-20 23:24:46 +0300

    simple SSL setup

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,10 +3,37 @@ package com.example
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.network.tls.certificates.*
 import com.example.plugins.*
+import org.slf4j.LoggerFactory
+import java.io.File
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    val keyStoreFile = File("keystore.jks")
+    val keystore = generateCertificate(
+        file = keyStoreFile,
+        keyAlias = "lvl_up_ssl",
+        keyPassword = "TempPswd0!",
+        jksPassword = "TempPswd0!"
+    )
+
+    val environment = applicationEngineEnvironment {
+        log = LoggerFactory.getLogger("ktor.application")
+        connector {
+            port = 8080
+        }
+        sslConnector(
+            keyStore = keystore,
+            keyAlias = "lvl_up_ssl",
+            keyStorePassword = { "TempPswd0!".toCharArray() },
+            privateKeyPassword = { "TempPswd0!".toCharArray() }) {
+            port = 8443
+            keyStorePath = keyStoreFile
+        }
+        module(Application::module)
+    }
+
+    embeddedServer(Netty, environment = environment)
         .start(wait = true)
 }
 
