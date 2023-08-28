f5806337b32fffb2a9a899010f62003d050da1d9
Igor Pelevin <ia_pelevin@mail.ru>
2023-08-28 22:43:12 +0300

    trip likes

--- src/main/kotlin/com/example/data/model/request/Trip.kt
+++ src/main/kotlin/com/example/data/model/request/Trip.kt
@@ -1,9 +1,10 @@
 package com.example.data.model.request
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TripLikeRequest(
-    val tripId: String,
+    @SerialName("trip_id") val tripId: String,
     val liked: Boolean,
 )
